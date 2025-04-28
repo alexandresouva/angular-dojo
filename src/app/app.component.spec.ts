a8b9f8eaf5f865ab461260f6c7234d606d9c184b
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-04-27 22:45:30 -0300

    chore: remove all content from the project base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'angular-dojo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-dojo');
+    expect(app.title).toEqual('Angular Dojo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-dojo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Dojo');
   });
 });
