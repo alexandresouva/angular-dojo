f9d0c2d807928e65789e76e1e87b82729fcd4953
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-05-16 22:21:46 -0300

    feat: add ContentComponent with ng-content support and integrate into dojo-components

---
+++ src/app/in-depth-guide/components/content/content.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dojo-content',
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.scss',
+})
+export class ContentComponent {}
--- src/app/topics-containers/components/components.component.ts
+++ src/app/topics-containers/components/components.component.ts
@@ -2,10 +2,16 @@ import { Component, effect, signal } from '@angular/core';
 import { SelectorComponent } from '../../in-depth-guide/components/selector/selector.component';
 import { InputComponent } from '../../in-depth-guide/components/input/input.component';
 import { OutputComponent } from '../../in-depth-guide/components/output/output.component';
+import { ContentComponent } from '../../in-depth-guide/components/content/content.component';
 
 @Component({
   selector: 'dojo-components',
-  imports: [SelectorComponent, InputComponent, OutputComponent],
+  imports: [
+    SelectorComponent,
+    InputComponent,
+    OutputComponent,
+    ContentComponent,
+  ],
   templateUrl: './components.component.html',
   styleUrl: './components.component.scss',
 })
